22abb168e5f414519ad2da305670da307fc5eddb
h3po41001 <h3po41001@gmail.com>
2024-10-21 20:35:38 +0900

    sdf

--- CressemCADViewer/CressemCADViewer/ViewModel/MainViewModel.cs
+++ CressemCADViewer/CressemCADViewer/ViewModel/MainViewModel.cs
@@ -4,7 +4,6 @@ using System.Windows;
 using CressemCADViewer.Model;
 using CressemCADViewer.ViewModel.Control;
 using CressemDataToGraphics;
-using CressemDataToGraphics.Converter;
 using CressemDataToGraphics.Model;
 using CressemExtractLibrary;
 using CressemExtractLibrary.Data;
@@ -83,8 +82,8 @@ namespace CressemCADViewer.ViewModel
 			// 데이터 상에는 자기 자신의 값 형태 있음
 			bool useMM = true;
 
-			var profile = ExtractLibrary.Instance.GetStepRoi("STRIP");		
-			var features = ExtractLibrary.Instance.GetFeatures("STRIP", "L01");
+			var profile = ExtractLibrary.Instance.GetStepRoi("UNIT");		
+			var features = ExtractLibrary.Instance.GetFeatures("UNIT", "L01");
 			DataToGraphics dataToGraphics = new DataToGraphics(10.0f, GraphicsType.GdiPlus);
 
 			GraphicsView.ClearShape();
@@ -93,104 +92,7 @@ namespace CressemCADViewer.ViewModel
 			foreach (var feature in features)
 			{
 				GraphicsView.AddShape(dataToGraphics.GetShape(useMM, feature));
-
-				//bool isFill = feature.Polarity.Equals("P") is true;
-
-				//if (feature is IFeatureSurface surface)
-				//{
-				//	foreach (var polygon in surface.Polygons)
-				//	{
-				//		bool isIsland = polygon.PolygonType.Equals("I");
-				//		List<IShapeBase> shapes = new List<IShapeBase>();
-
-				//		foreach (var polyFeature in polygon.Features)
-				//		{
-				//			if (polyFeature is IFeatureLine lineFeature)
-				//			{
-				//				shapes.Add(ShapeLine.Create(10.0f, lineFeature));									
-				//			}
-				//			else if (polyFeature is IFeatureArc arcFeature)
-				//			{
-				//				shapes.Add(ShapeArc.Create(10.0f, arcFeature));
-				//			}
-				//		}
-
-				//		// shapePolygon.Shapes = shapes;
-				//	}
-
-
-				//	// GraphicsView.AddShape(shapePolygon);
-				//}
 			}
-
-			//if (PropertyView.SelectedStepName is null)
-			//{
-			//	return;
-			//}
-
-			//if (PropertyView.SelectedLayerName is null)
-			//{
-			//	return;
-			//}
-
-			//RectangleF stepRoi = ExtractLibrary.Instance.GetStepRoi(PropertyView.SelectedStepName);
-			//GraphicsView.LoadRoi(DrawingFactory.Instance.GetGdiRoi(stepRoi), 10.0f);
-
-			//var features = ExtractLibrary.Instance.GetFeatures(
-			//	PropertyView.SelectedStepName,
-			//	PropertyView.SelectedLayerName);
-
-			//GraphicsView.AddShape(DrawingFactory.Instance.GetGdiLine(
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.10964587),
-			//		(float)Converter.Instance.ConvertInchToMM(0.21291939)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.11013169),
-			//		(float)Converter.Instance.ConvertInchToMM(0.21192992))));
-
-			//GraphicsView.AddShape(DrawingFactory.Instance.GetGdiArc(
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(0.209586614173),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.216535433071)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(0.209586614173),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.216535433071)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(0.216535433071),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.216535433071))));
-
-			//GraphicsView.AddShape(DrawingFactory.Instance.GetGdiArc(
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.262854330709),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.216535433071)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.262854330709),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.216535433071)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.255905511811),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.216535433071))));
-
-			//GraphicsView.AddShape(DrawingFactory.Instance.GetGdiArc(
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.262854330709),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.196850393701)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.262854330709),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.196850393701)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.255905511811),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.196850393701))));
-
-			//GraphicsView.AddShape(DrawingFactory.Instance.GetGdiArc(
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.261415354331),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.255692519685)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.261415354331),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.255692519685)),
-			//	new PointF(
-			//		(float)Converter.Instance.ConvertInchToMM(-0.260332677165),
-			//		(float)Converter.Instance.ConvertInchToMM(-0.255692519685))));
 		}
 
 		private void LogoView_LogoDoubleClickedEvent(object sender, EventArgs e)
